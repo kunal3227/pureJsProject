9aeddfe988ef2a292567434194578d9daa86e89f
kunal3227 <kunalanand3227@gmail.com>
2025-07-13 18:08:31 +0530

    operator done

---
+++ filetwo.js
@@ -0,0 +1,60 @@
+// let score = undefined
+
+// // console.log(typeof score);
+// // console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1 & false -> 0
+// null -> 0 | NaN
+// undefined -> NaN
+
+// let isLoggedIn = ""
+
+// let boolIsLoggedIn = Boolean(isLoggedIn)
+// console.log(boolIsLoggedIn);
+
+// // 0 => false , 1 => true
+// // "" => false, "fdfa" => true
+// // null , undefined => false
+
+
+// let someName = undefined
+
+// let stringSomeName = String(someName)
+// console.log(typeof stringSomeName);
+
+//null => null (string)
+//undefined => undefined(string)
+
+// ****************************Operation***********************************
+
+let value = 3
+negValue = -value
+//console.log(negValue + 13);
+
+// let str1 = "hello"
+// let str2 = " kunal"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(typeof("1"+2));
+// console.log(typeof("1"+2+2));
+// console.log(typeof(1+2+"2"));
+
+// console.log(-false);
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 4
+
+let gameCounter = 100
+++gameCounter
+gameCounter++
+console.log(gameCounter);
+
+
