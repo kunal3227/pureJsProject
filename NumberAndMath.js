318afb33f436f79f2c6528318c16806544963134
kunal3227 <kunalanand3227@gmail.com>
2025-07-15 09:44:58 +0530

    Num and Math completed

---
+++ NumberAndMath.js
@@ -0,0 +1,28 @@
+// const score = 400
+// console.log(score);
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(5));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString());
+
+//+++++++++++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++
+
+// console.log(Math.abs(-23))
+// console.log(Math.round(4.67));
+// console.log(Math.ceil(4.01));
+// console.log(Math.floor(4.9999999));
+// console.log(Math.min(4,3,6,7,8,9));
+
+console.log(Math.random());
+
+const max = 20
+const min = 12
+
+console.log(Math.floor(Math.random() * (max - min + 1))+min)
